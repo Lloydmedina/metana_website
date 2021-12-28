28c3c2697dd3f09f3b4c6f4a2aebb7e5b864752c
Lloydmedina <79982709+Lloydmedina@users.noreply.github.com>
2021-12-28 14:26:37 +0800

    update files

--- src/components/Logo/LogoStyle.js
+++
@@ -1,9 +0,0 @@
-import { IoIosArrowDropdown } from 'react-icons/io';
-import styled from 'styled-components';
-
-// export const Metalogo = styled.div`
-// width: 50%;
-// height: 50%;
-// display: flex;
-// position: relative;
-// `;
